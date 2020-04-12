3e6136f51f1c3fd298edbf2e6124a5bbb15f16c9
dirhazg <dirhazg@gmail.com>
2020-04-12 19:23:01 -0400

    .

--- scripts/bank.js
+++ scripts/bank.js
@@ -992,7 +992,7 @@ function BankReader() {
 		if (a1lib.simplecompare(buffer, BankReader.placeholderzero, 2, 1) !== false) {
 			return -1;
 		}
-		
+		qw(buffer.toString());
 		while (anymatch && dx < slotsize) {
 			anymatch = false;
 			for (var a = 0; a < BankReader.itemnumbers.buffers.length; a++) {

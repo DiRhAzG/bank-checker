5e1dd2671973def957d5585c4f820bee6b669a17
dirhazg <dirhazg@gmail.com>
2020-04-12 13:32:31 -0400

    .

--- scripts/bank.js
+++ scripts/bank.js
@@ -323,7 +323,7 @@ function BankReader() {
 	this.readItems = function (buffer) {
 		qw("reading Items");
 		qw("Columns: " + me.pos.columns);
-		qw("Rows: " + state.rows.length);
+		qw("Rows: " + me.state.rows.length);
 		readcount++;
 		var allvalid = true;
 		var hadempty = false;

cbf92ae3186e7428634706d436defe37c836df19
dirhazg <dirhazg@gmail.com>
2020-04-15 23:10:06 -0400

    .

--- scripts/materialstorage.js
+++ scripts/materialstorage.js
@@ -137,6 +137,24 @@ function MaterialsReader() {
 		//can not have a tooltip, added benifit of ignoring it when user is searching
         var searchbuf = img.toData(me.pos.area.x + me.pos.area.w - 48, me.pos.area.y + me.pos.area.h - 38, MaterialsReader.botright.width, MaterialsReader.botright.height);
         
+              // create off-screen canvas element
+		var canvas = document.createElement('canvas'),
+		ctx = canvas.getContext('2d');
+
+		canvas.width = searchbuf.width;
+		canvas.height = searchbuf.height;
+
+		// create imageData object
+		var idata = ctx.createImageData(searchbuf.width, searchbuf.height);
+
+		// set our buffer as source
+		idata.data.set(searchbuf.data);
+
+		// update canvas with new data
+		ctx.putImageData(idata, 0, 0);
+        var dataUri = canvas.toDataURL(); // produces a PNG file
+        console.log(dataUri);
+
 		var isopen = a1lib.simplecompare(searchbuf, MaterialsReader.botright, 0, 0) !== false;
 		if (!isopen) {
 			if (isopen != me.isopen) {

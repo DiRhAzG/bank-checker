52ac1a6cdafb7b78c0da08cdce2431ea80d905ab
dirhazg <dirhazg@gmail.com>
2020-04-18 09:58:07 -0400

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -500,7 +500,7 @@ function buildTable() {
 			$(".mats").append(`
 				<div class='row' data-name="${name}">
 					<div class="col hide"><input type="checkbox" class="hideMe" ${mat.hide ? "checked=checked" : ""}/></div>
-					<div class='col-4' title="\nLevel: ${mat.level}\nFaction: ${mat.faction}\nLocation(s):\n${mat.location}">
+					<div class='col-6' title="\nLevel: ${mat.level}\nFaction: ${mat.faction}\nLocation(s):\n${mat.location}">
 						${mat.name}
 					</div>
 					<div class="mat-col">
